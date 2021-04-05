fbe131107904e346172d9e7de18901043bdd2343
Liam Murphy <liamphmurphy@gmail.com>
2021-04-04 23:09:32 -0700

    separate out some logic for file name and content length

--- main.py
+++ main.py
@@ -7,7 +7,22 @@ class File:
     def __init__(self, name, size):
         self.name = name
         self.size = size
+
+
+def find_end_of_name(start_count: int, data: list) -> int:
+    count = start_count
+    # the string 'UT' marks the end of a file name
+    while data[count] != b'U' and data[count + 1] != b'T':
+        count += 1
+    return count
     
+def find_end_of_data(start_count: int, data: list) -> int:
+    count = start_count
+    length = 0
+    while data[count] != b'\n':
+        count += 1
+        length += 1
+    return length
 
 # open file and read in byte by byte
 with open("files.zip", "rb") as f:
@@ -25,8 +40,13 @@ for count in range(len(byte_list)):
     try: 
         # detect start of a new local file header
         if byte_list[count] == b'P' and byte_list[count + 1] == b'K':
-            name = byte_list[count + 30:count + 35]
-            print(name)
+            end_name_index = find_end_of_name(count + 30, byte_list)
+            data = find_end_of_data(count + 63, byte_list)
+            name = byte_list[count + 30:end_name_index]
+            print(name, data)
+            #print(byte_list[count+63:count+67])
+
+        #print(count, byte_list[count].decode('utf-8'), byte_list[count])
     except Exception as e: 
         pass # ignore any errors
     count += 1
